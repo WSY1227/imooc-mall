5de648f85210897cd60d2b1008ab486c3401d2ed
xu <1669228230>
2022-09-07 15:38:54 +0800

    优化

--- src/main/java/com/imooc/mall/controller/CategoryController.java
+++ src/main/java/com/imooc/mall/controller/CategoryController.java
@@ -20,16 +20,12 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 描述：     目录Controller
  */
-@Controller
+@RestController
 public class CategoryController {
 
     @Autowired
@@ -43,50 +39,22 @@ public class CategoryController {
      */
     @ApiOperation("后台添加目录")
     @PostMapping("admin/category/add")
-    @ResponseBody
-    public ApiRestResponse addCategory(HttpSession session,
-                                       @Valid @RequestBody AddCategoryReq addCategoryReq) {
-        User currentUser = (User) session.getAttribute(Constant.IMOOC_MALL_USER);
-        if (currentUser == null) {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_LOGIN);
-        }
-        //校验是否是管理员
-        boolean adminRole = userService.checkAdminRole(currentUser);
-        if (adminRole) {
-            //是管理员，执行操作
-            categoryService.add(addCategoryReq);
-            return ApiRestResponse.success();
-        } else {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_ADMIN);
-        }
+    public ApiRestResponse addCategory(@Valid @RequestBody AddCategoryReq addCategoryReq) {
+        categoryService.add(addCategoryReq);
+        return ApiRestResponse.success();
     }
 
     @ApiOperation("后台更新目录")
     @PostMapping("admin/category/update")
-    @ResponseBody
-    public ApiRestResponse updateCategory(@Valid @RequestBody UpdateCategoryReq updateCategoryReq,
-                                          HttpSession session) {
-        User currentUser = (User) session.getAttribute(Constant.IMOOC_MALL_USER);
-        //若为空则没登陆
-        if (currentUser == null) {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_LOGIN);
-        }
-        //校验是否是管理员
-        boolean adminRole = userService.checkAdminRole(currentUser);
-        if (adminRole) {
-            //是管理员，执行操作
-            Category category = new Category();
-            BeanUtils.copyProperties(updateCategoryReq, category);
-            categoryService.update(category);
-            return ApiRestResponse.success();
-        } else {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_ADMIN);
-        }
+    public ApiRestResponse updateCategory(@Valid @RequestBody UpdateCategoryReq updateCategoryReq) {
+        Category category = new Category();
+        BeanUtils.copyProperties(updateCategoryReq, category);
+        categoryService.update(category);
+        return ApiRestResponse.success();
     }
 
     @ApiOperation("后台删除目录")
     @PostMapping("admin/category/delete")
-    @ResponseBody
     public ApiRestResponse deleteCategory(@RequestParam Integer id) {
         categoryService.delete(id);
         return ApiRestResponse.success();
@@ -94,7 +62,6 @@ public class CategoryController {
 
     @ApiOperation("后台目录列表")
     @GetMapping("admin/category/list")
-    @ResponseBody
     public ApiRestResponse listCategoryForAdmin(@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
         PageInfo pageInfo = categoryService.listForAdmin(pageNum, pageSize);
         return ApiRestResponse.success(pageInfo);
@@ -102,7 +69,6 @@ public class CategoryController {
 
     @ApiOperation("前台目录列表")
     @GetMapping("category/list")
-    @ResponseBody
     public ApiRestResponse listCategoryForCustomer() {
         List<CategoryVO> categoryVOS = categoryService.listCategoryForCustomer(0);
         return ApiRestResponse.success(categoryVOS);
